6d6b24ecf22efbe677da906de135985ec3bd4a6e
Orlando Caesar Donesa <orlandocaesardonesa@gmail.com>
2026-06-23 23:11:47 +0800

    chore: initialize Expo app with Nativewind setup

---
+++ src/app/_layout.tsx
@@ -0,0 +1,12 @@
+import "@/global.css";
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    />
+  );
+}
---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 bg-black items-center justify-center">
+      <Text className="text-4xl font-bold text-blue-500">ZapZone</Text>
+    </View>
+  );
+}
