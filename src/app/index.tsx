41c98555788369f50e03088d0f18513ed11c7375
EdzZ29 <jrederio01@gmail.com>
2026-06-24 06:57:47 +0800

    Update: Reset project to new start

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,9 @@
 import { Text, View } from "react-native";
 
-export default function HomeScreen() {
+export default function Index() {
   return (
-    <View className="flex-1 bg-black items-center justify-center">
-      <Text className="text-4xl font-bold text-blue-500">ZapZone</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-6xl font-bold text-blue-500">ZapZone</Text>
     </View>
   );
 }
