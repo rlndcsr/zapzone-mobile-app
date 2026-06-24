d4b2d77b7b4872008fa69115049532ea1661582a
EdzZ29 <jrederio01@gmail.com>
2026-06-24 09:22:06 +0800

    Updated: Install packages and add the logo assests

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,43 @@
-import { Text, View } from "react-native";
+import { Text, View, Animated, Easing } from "react-native";
+import { Image } from "expo-image";
+import { useEffect, useRef } from "react";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function Index() {
+
+  const translateYAnim = useRef(new Animated.Value(100)).current;
+  const opacityAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.parallel([
+      Animated.timing(opacityAnim, {
+        toValue: 1,
+        duration: 800,
+        easing: Easing.out(Easing.cubic),
+        useNativeDriver: true,
+      }),
+      Animated.timing(translateYAnim, {
+        toValue: 0,
+        duration: 800,
+        easing: Easing.out(Easing.cubic),
+        useNativeDriver: true,
+      }),
+    ]).start();
+  }, [translateYAnim, opacityAnim]);
+
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-6xl font-bold text-blue-500">ZapZone</Text>
-    </View>
+    <LinearGradient colors={["#032368", "#0642C4"]} start={{ x: 1, y: 1 }} end={{ x: 0, y: 0 }} style={{ flex: 1 }}>
+      <View className="flex-1 items-center justify-center">
+        <Animated.View style={{opacity: opacityAnim, transform: [{ translateY: translateYAnim }],}}>
+          <Image
+            //logo 
+            source={require("../../assets/zapzone-assests/zapzone.png")}
+            style={{width: 64, height: 64,}}contentFit="contain"/>
+        </Animated.View>
+
+        <Animated.View style={{opacity: opacityAnim,transform: [{ translateY: translateYAnim }],marginTop: 40,}}></Animated.View>
+          
+      </View>
+    </LinearGradient>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#032368",
+        secondary: "#0642C4",
+        accent: "#EAE22F",
+        font: "#242424",
+      },
+    },
   },
   plugins: [],
 };
